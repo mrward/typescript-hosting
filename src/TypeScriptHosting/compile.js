65a1a46b7af8eb7ea4906f81c5ba14159b94cc86
Matt Ward <ward.matt@gmail.com>
2014-07-29 20:28:25 +0100

    Get semantic errors back from the compiler.
    TypeScript compiler has a getSemanticDiagnostics() method that takes
    a filename and returns any errors in the TypeScript file. The
    emit method that generates the JavaScript file does not return these
    errors.

--- src/TypeScriptHosting/compile.js
+++ src/TypeScriptHosting/compile.js
@@ -2,5 +2,19 @@
 var hostAdapter = new TypeScript.Services.LanguageServiceShimHostAdapter(host);
 var compiler = new TypeScript.Services.LanguageServiceCompiler(hostAdapter);
 var emitResult = compiler.emit(host.FileName, host.ResolvePath);
+host.log("compiler.emit complete");
 var result = JSON.stringify({result: emitResult});
 host.updateCompilerResult(result);
+host.log("compiler.getSemanticDiagnostics start");
+var diag = compiler.getSemanticDiagnostics(host.FileName);
+var result2 = JSON.stringify({result: diag});
+host.log("compiler.getSemanticDiagnostics complete");
+host.log(result2);
+
+//var diag2 = compiler.getSyntacticDiagnostics(host.FileName);
+//var result3 = JSON.stringify({result: diag2});
+//host.log("getSyntacticDiagnostics");
+//host.log(result3);
+
+
+
